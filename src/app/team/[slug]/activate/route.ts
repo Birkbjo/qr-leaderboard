24b4cd4a264667ac044bdd2732d211c2fa3c3c44
Birk Johansson <birk@dhis2.org>
2025-03-05 01:24:02 +0100

    parallel activation

--- src/app/team/[slug]/activate/route.ts
+++ src/app/team/[slug]/activate/route.ts
@@ -9,33 +9,33 @@ import { NextRequest } from "next/server";
 // this is a route to set the session cookie
 // cant do that in server-component
 export async function GET(request: NextRequest, nextPageProps: NextPageProps) {
-    const supabase = await createClient()
+    const supabase = await createClient();
     const { slug } = await nextPageProps.params;
     const loggedIn = await getSession();
-    console.log('in activate', loggedIn)
     if (loggedIn?.teamId) {
-        console.log('loggedIn', loggedIn);
         redirect(`/team`);
     }
 
-    console.log('get team')
-    const { data: team } = await supabase.from('team').select('*').eq('id', slug).single()
-    console.log('team', team)
+    const { data: team } = await supabase
+        .from("team")
+        .select("*")
+        .eq("id", slug)
+        .single();
     if (!team) {
         return redirect("/");
     }
-    const isAdmin = team.name === 'Admin';
-    console.log('create session')   
+    const isAdmin = team.name === "Admin";
     await createSession(team.id, isAdmin);
 
     if (!team.activated && !isAdmin) {
-        console.log('activate team')
-        await supabase.from('team').update({ activated: true }).eq('id', team.id)
-        await supabase.from('activity').insert({
-            team: team.id,
-            challenge: 'b2c923b8-2c9b-4e5a-84b8-0fb54fbe2089'
-        })
+        const promises = [
+            supabase.from("team").update({ activated: true }).eq("id", team.id),
+            supabase.from("activity").insert({
+                team: team.id,
+                challenge: "b2c923b8-2c9b-4e5a-84b8-0fb54fbe2089",
+            }),
+        ];
+        await Promise.all(promises);
     }
-    console.log('redirect to teampage')
     redirect(`/team`);
 }
