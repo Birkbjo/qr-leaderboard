e04ef08c4c965903cd5388f34385e316d2dd93da
Birk Johansson <birk@dhis2.org>
2025-03-05 16:49:53 +0100

    fix: challenge qr path

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -48,7 +48,7 @@ export default async function AdminPage() {
                                 </CardHeader>
                                 <CardContent className="p-4 mt-4">
                                     <div className="flex flex-col items-center space-y-4">
-                                        <QRCode url={`/team/${challenge.id}`} />
+                                        <QRCode url={`/challenge/${challenge.id}`} />
                                     </div>
                                 </CardContent>
                             </div>
